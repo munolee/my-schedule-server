8e0b87acb078e24c75df7ef4c866144fb421f743
Ian <yyh_lee@fitpet.co.kr>
2023-03-06 19:43:31 +0900

    🩹 공휴일 OpenApi 호출 수정
    - try catch문 추가

--- dist/api/schedule.js
+++ dist/api/schedule.js
@@ -20,41 +20,41 @@ const router = express_1.default.Router();
 const parser = new fast_xml_parser_1.XMLParser();
 const serviceKey = 'UqfEaWsllhXKcqR1XHjqNFvwf75vk1k4HOd4IAh6eK1aIK75vhki%2FgEuhDeo%2BEe3hp1%2F%2BvHRLeGjM9ZTFQfVIA%3D%3D';
 const url = `https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo?serviceKey=${serviceKey}`;
-let holidayJson = {};
 /** /api/schedule Endpoint **/
 router.get('/', (req, res) => {
     // 공공 데이터 포탈 공휴일 데이터 요청 보내기
     const year = req.query.year || '2023';
     request(url + `&solYear=${year}`, (error, response, body) => __awaiter(void 0, void 0, void 0, function* () {
-        if (!error && response.statusCode == 200) {
+        try {
             yield new Promise(() => {
-                holidayJson = parser.parse(body).response.body.items.item;
+                const holidayJson = parser.parse(body).response.body.items.item;
+                // 가져온 데이터를 형식에 맞게 파싱하기
+                const holidaySchedule = Object.values(holidayJson).map((data) => {
+                    return {
+                        startDate: (data === null || data === void 0 ? void 0 : data.locdate).toString().replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
+                        endDate: (data === null || data === void 0 ? void 0 : data.locdate).toString().replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
+                        eventTitle: data === null || data === void 0 ? void 0 : data.dateName,
+                        typeId: 2,
+                        bgColor: "#EDAA7D"
+                    };
+                });
+                // Mock 데이터와 병합, 정렬 후 데이터 내보내기
+                const schedules = [...holidaySchedule, ...mock_1.schedule].sort((a, b) => {
+                    if (a.startDate >= b.startDate)
+                        return 1;
+                    if (a.endDate >= b.endDate)
+                        return -1;
+                    return -1;
+                });
+                return res.json(schedules);
             });
         }
-        else {
+        catch (error) {
             console.error(error);
-            res.status(500).send('Something went wrong');
+            // 에러 발생 시 Mock 데이터만 내보내기
+            return res.json(mock_1.schedule);
         }
     }));
-    // 가져온 데이터를 형식에 맞게 파싱하기
-    const holidaySchedule = Object.values(holidayJson).map((data) => {
-        return {
-            startDate: (data.locdate).toString().replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
-            endDate: (data.locdate).toString().replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
-            eventTitle: data.dateName,
-            typeId: 2,
-            bgColor: "#EDAA7D"
-        };
-    });
-    // Mock 데이터와 병합, 정렬 후 데이터 내보내기
-    const schedules = [...holidaySchedule, ...mock_1.schedule].sort((a, b) => {
-        if (a.startDate >= b.startDate)
-            return 1;
-        if (a.endDate >= b.endDate)
-            return -1;
-        return -1;
-    });
-    return res.json(schedules);
 });
 module.exports = router;
 //# sourceMappingURL=schedule.js.map
--- src/api/schedule.ts
+++ src/api/schedule.ts
@@ -10,7 +10,6 @@ const parser = new XMLParser();
 
 const serviceKey =  'UqfEaWsllhXKcqR1XHjqNFvwf75vk1k4HOd4IAh6eK1aIK75vhki%2FgEuhDeo%2BEe3hp1%2F%2BvHRLeGjM9ZTFQfVIA%3D%3D'
 const url = `https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo?serviceKey=${serviceKey}`
-let holidayJson = {}
 
 /** /api/schedule Endpoint **/
 router.get('/', (req: Request, res: Response) => {
@@ -18,34 +17,35 @@ router.get('/', (req: Request, res: Response) => {
     // 공공 데이터 포탈 공휴일 데이터 요청 보내기
     const year = req.query.year || '2023';
     request(url + `&solYear=${year}`, async (error: Error, response: Response, body: string | Buffer) => {
-        if (!error && response.statusCode == 200) {
-            await new Promise(() => {
-                holidayJson = parser.parse(body).response.body.items.item
+        try {
+        await new Promise(() => {
+            const holidayJson = parser.parse(body).response.body.items.item
+
+            // 가져온 데이터를 형식에 맞게 파싱하기
+            const holidaySchedule: ScheduleType[] = Object.values(holidayJson).map((data: HolidayJsonType) => {
+                return {
+                    startDate: (data?.locdate).toString().replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
+                    endDate: (data?.locdate).toString().replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
+                    eventTitle: data?.dateName,
+                    typeId: 2,
+                    bgColor: "#EDAA7D"
+                }
             })
-        } else {
-            console.error(error);
-            res.status(500).send('Something went wrong');
+
+            // Mock 데이터와 병합, 정렬 후 데이터 내보내기
+            const schedules = [...holidaySchedule, ...schedule].sort((a, b) => {
+                if (a.startDate >= b.startDate) return 1;
+                if (a.endDate >= b.endDate) return -1;
+                return -1;
+            })
+            return res.json(schedules);
+        })
+    } catch (error) {
+        console.error(error);
+        // 에러 발생 시 Mock 데이터만 내보내기
+        return res.json(schedule);
         }
     });
-
-    // 가져온 데이터를 형식에 맞게 파싱하기
-    const holidaySchedule: ScheduleType[] = Object.values(holidayJson).map((data: HolidayJsonType) => {
-        return {
-            startDate: (data.locdate).toString().replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
-            endDate: (data.locdate).toString().replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
-            eventTitle: data.dateName,
-            typeId: 2,
-            bgColor: "#EDAA7D"
-        }
-    })
-
-    // Mock 데이터와 병합, 정렬 후 데이터 내보내기
-    const schedules = [...holidaySchedule, ...schedule].sort((a, b) => {
-        if (a.startDate >= b.startDate) return 1;
-        if (a.endDate >= b.endDate) return -1;
-        return -1;
-    })
-    return res.json(schedules);
 })
 
 module.exports = router;
